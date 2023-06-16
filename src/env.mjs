3d62edb476dc296ed6703a110997b107211420eb
Gal Gueta <galg@cynet.com>
2023-06-16 18:44:49 +0300

    basic configurations

--- src/env.mjs
+++ src/env.mjs
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    PLAYLISTS_API_KEY: z.string(),
   },
 
   /**
@@ -25,6 +26,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    PLAYLISTS_API_KEY: process.env.PLAYLISTS_API_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
