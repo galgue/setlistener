1940a783c142cb161ada39dab9a1d0f611df9272
Gal Gueta <galg@cynet.com>
2023-06-17 16:32:16 +0300

    add spotify artist schema

--- src/server/spotifyApi/Spotify.schema.ts
+++ src/server/spotifyApi/Spotify.schema.ts
@@ -5,3 +5,27 @@ export const SpotifyTokenSchema = z.object({
   token_type: z.string(),
   expires_in: z.number(),
 });
+
+export const SpotifyArtistSchema = z.object({
+  external_urls: z.object({
+    spotify: z.string(),
+  }),
+  followers: z.object({
+    href: z.null(),
+    total: z.number(),
+  }),
+  genres: z.array(z.string()),
+  href: z.string(),
+  id: z.string(),
+  images: z.array(
+    z.object({
+      height: z.number(),
+      url: z.string(),
+      width: z.number(),
+    })
+  ),
+  name: z.string(),
+  popularity: z.number(),
+  type: z.string(),
+  uri: z.string(),
+});
