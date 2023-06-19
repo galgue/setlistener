089b2050a4964bcf06e76c0792a27668c04ad31f
Gal Gueta <galg@cynet.com>
2023-06-19 19:19:13 +0300

    add missing type for spotify api

--- src/server/spotifyApi/Spotify.schema.ts
+++ src/server/spotifyApi/Spotify.schema.ts
@@ -10,22 +10,64 @@ export const SpotifyArtistSchema = z.object({
   external_urls: z.object({
     spotify: z.string(),
   }),
-  followers: z.object({
-    href: z.null(),
-    total: z.number(),
-  }),
-  genres: z.array(z.string()),
+  genres: z.array(z.string()).optional(),
   href: z.string(),
   id: z.string(),
-  images: z.array(
-    z.object({
-      height: z.number(),
-      url: z.string(),
-      width: z.number(),
-    })
-  ),
+  images: z
+    .array(
+      z.object({
+        height: z.number(),
+        url: z.string(),
+        width: z.number(),
+      })
+    )
+    .optional(),
   name: z.string(),
-  popularity: z.number(),
+  popularity: z.number().optional(),
   type: z.string(),
   uri: z.string(),
 });
+
+export const SongSchema = z.object({
+  album: z.object({
+    album_type: z.string(),
+    external_urls: z.object({
+      spotify: z.string(),
+    }),
+    href: z.string(),
+    id: z.string(),
+    images: z.array(
+      z.object({
+        height: z.number(),
+        url: z.string(),
+        width: z.number(),
+      })
+    ),
+    name: z.string(),
+    release_date: z.string(),
+    release_date_precision: z.string(),
+    total_tracks: z.number(),
+    type: z.string(),
+    uri: z.string(),
+  }),
+  artists: z.array(SpotifyArtistSchema),
+  disc_number: z.number(),
+  duration_ms: z.number(),
+  explicit: z.boolean(),
+  external_ids: z.object({
+    isrc: z.string(),
+  }),
+  external_urls: z.object({
+    spotify: z.string(),
+  }),
+  href: z.string(),
+  id: z.string(),
+  is_local: z.boolean(),
+  name: z.string(),
+  popularity: z.number(),
+  preview_url: z.string().nullable().optional(),
+  type: z.string(),
+  uri: z.string(),
+});
+
+export type SpotifySong = z.infer<typeof SongSchema>;
