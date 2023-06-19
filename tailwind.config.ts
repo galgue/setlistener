6425621cdb1f8c1cb7c54699ea236a9a43910e3d
Gal Gueta <galg@cynet.com>
2023-06-19 19:16:47 +0300

    add colors to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        spotify: {
+          background: "#000000",
+          header: "#232323",
+          row: "#121212",
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
