7ca6c28bd67a2cbe0b18f497154901fd2b031eef
Gal Gueta <galg@cynet.com>
2023-06-20 22:44:01 +0300

    add songs list

---
+++ src/app/artist/playlist/SongsList.tsx
@@ -0,0 +1,40 @@
+import { apiCaller } from "~/server/api/root";
+import { SongInfo } from "./SongInfo";
+
+type SongsListProps = {
+  artistId: string;
+  top?: number;
+  tour?: string;
+  withCovers?: string;
+  minOccurrences?: number;
+  artistName: string;
+};
+
+export const SongsList = async ({
+  artistId,
+  top,
+  tour,
+  minOccurrences,
+  withCovers,
+  artistName,
+}: SongsListProps) => {
+  const songs = await apiCaller.artist.songsOfLastShows({
+    artistId,
+    top,
+    tour,
+    minOccurrences,
+    withCovers,
+  });
+
+  return (
+    <div className="h-full w-full overflow-scroll">
+      {songs.map((song) => (
+        <SongInfo
+          key={`${song.name}-${song.cover ?? artistName}`}
+          artist={song.cover ?? artistName}
+          songName={song.name}
+        />
+      ))}
+    </div>
+  );
+};
