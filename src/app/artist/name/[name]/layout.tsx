dfa70fbf147e6a032e2cf117877d445382635488
Gal Gueta <galg@cynet.com>
2023-06-21 22:18:29 +0300

    use page with header in artist search

---
+++ src/app/artist/name/[name]/layout.tsx
@@ -0,0 +1,10 @@
+import { PageWithHeader } from "~/components/pageWithHeader";
+import "~/styles/globals.css";
+
+export const metadata = {
+  title: "Select Artist",
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <PageWithHeader title={metadata.title}>{children}</PageWithHeader>;
+}
