5384ce9ba700b197948702de01bc09f008b19d42
Gal Gueta <galg@cynet.com>
2023-06-23 17:38:12 +0300

    add trpc client to use in client component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "~/styles/globals.css";
+import { ClientProvider } from "~/utils/trpcClient";
 
 export const metadata = {
   title: "Next.js",
@@ -11,14 +12,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="select-none">
-        <div className="flex h-screen items-center justify-center bg-spotify-header pb-10 md:pb-0">
-          <div className="relative h-[95vh] w-[95vw] rounded-lg bg-spotify-background px-4 py-4 shadow-md md:h-[40rem] md:w-96">
-            {children}
+    <ClientProvider>
+      <html lang="en">
+        <body className="select-none">
+          <div className="flex h-screen items-center justify-center bg-spotify-header pb-10 md:pb-0">
+            <div className="relative h-[95vh] w-[95vw] rounded-lg bg-spotify-background px-4 py-4 shadow-md md:h-[40rem] md:w-96">
+              {children}
+            </div>
           </div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    </ClientProvider>
   );
 }
