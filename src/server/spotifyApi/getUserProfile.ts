fdb08a26589025f95d376b26413f7d8d3197b757
Gal Gueta <galg@cynet.com>
2023-06-24 12:16:23 +0300

    remove log

--- src/server/spotifyApi/getUserProfile.ts
+++ src/server/spotifyApi/getUserProfile.ts
@@ -4,7 +4,6 @@ export const getUserProfile = async (
   tokenType: string,
   accessToken: string
 ) => {
-  console.log(accessToken);
   const response = await fetch("https://api.spotify.com/v1/me", {
     headers: {
       Authorization: `${tokenType} ${accessToken}`,
