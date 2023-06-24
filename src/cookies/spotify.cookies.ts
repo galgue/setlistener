e65deb13c1d0528c4d4b2992a7161aa015764f22
Gal Gueta <galg@cynet.com>
2023-06-24 12:13:03 +0300

    add spotify cookies names

---
+++ src/cookies/spotify.cookies.ts
@@ -0,0 +1,4 @@
+export const SPOTIFY_TOKEN = "spotify_token";
+export const SPOTIFY_REFRESH_TOKEN = "spotify_refresh_token";
+export const SPOTIFY_EXPIRES_AT = "spotify_expires_at";
+export const SPOTIFY_TOKEN_TYPE = "spotify_token_type";
