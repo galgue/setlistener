b81aa500d5b04d4e9536bc1fb11d6665cc4371ae
Gal Gueta <galg@cynet.com>
2023-06-24 13:15:22 +0300

    add retry to the get setlists

--- src/server/setlistApi/getSetlist.ts
+++ src/server/setlistApi/getSetlist.ts
@@ -1,18 +1,30 @@
 import { SetlistSchema } from "./Setlist.schemas";
 
 export const getSetlist = async (id: string) => {
-  const response = await fetch(
-    `https://api.setlist.fm/rest/1.0/setlist/${id}`,
-    {
-      next: {
-        revalidate: process.env.NODE_ENV === "production" ? 60 * 60 * 24 : 0,
-      },
-      headers: {
-        "x-api-key": process.env.PLAYLISTS_API_KEY as string,
-        Accept: "application/json",
-      },
+  let retries = 0;
+  while (retries < 3) {
+    const response = await fetch(
+      `https://api.setlist.fm/rest/1.0/setlist/${id}`,
+      {
+        next: {
+          revalidate: process.env.NODE_ENV === "production" ? 60 * 60 * 24 : 0,
+        },
+        headers: {
+          "x-api-key": process.env.PLAYLISTS_API_KEY as string,
+          Accept: "application/json",
+        },
+      }
+    );
+    const setlistResponse = SetlistSchema.safeParse(await response.json());
+
+    if (setlistResponse.success) {
+      return setlistResponse.data.sets;
     }
-  );
-  const setlistResponse = SetlistSchema.parse(await response.json());
-  return setlistResponse.sets;
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    retries++;
+  }
+
+  throw new Error("Could not fetch setlist");
 };
