beb86161ad7b03b24b7c4080537e2cc3135617cd
Gal Gueta <galg@cynet.com>
2023-06-30 23:59:18 +0300

    change name

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
 import { authOptions } from "~/lib/auth";
 
-type GetType = (request: Request) => Promise<Response>;
+type RequestType = (request: Request) => Promise<Response>;
 
-const handler = NextAuth(authOptions) as GetType;
+const handler = NextAuth(authOptions) as RequestType;
 
 export const GET = handler;
 export const POST = handler;
