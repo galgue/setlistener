ec2930402e494f7823d6a1f01caf1445af9eb68a
Gal Gueta <galg@cynet.com>
2023-07-04 13:33:37 +0300

    add apis to index

--- src/server/spotifyApi/index.ts
+++ src/server/spotifyApi/index.ts
@@ -1,3 +1,6 @@
 export * from "./createToken";
 export * from "./getArtistInfo";
 export * from "./getSong";
+export * from "./addPlaylistCoverImage";
+export * from "./addSongsToPlaylist";
+export * from "./createPlaylist";
